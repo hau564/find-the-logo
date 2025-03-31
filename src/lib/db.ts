cd743135d50e4cc083a5c99166b6253709758639
hau564 <thanhhau050604@gmail.com>
2025-03-31 08:20:40 +0700

    Add PORT to database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,6 +8,7 @@ console.log(process.env.DB_HOST);
 // Define connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
